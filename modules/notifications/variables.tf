6eb64ab498cae20c98fdc1bf1f6996444d2bbc84
kuchurisatwik <kuchurisatwik@gmail.com>
2025-12-17 17:55:08 +0530

    variables_

--- modules/notifications/variables.tf
+++ modules/notifications/variables.tf
@@ -3,3 +3,9 @@ variable "alert_email" {
   description = "The email address to receive notifications"
   type        = string
 }
+
+variable "codestar_connection_arn" {
+  description = "ARN of the AWS CodeStar connection to GitHub"
+  type        = string
+  default = "arn:aws:codeconnections:us-east-1:269196137888:connection/4ffc60ef-8acd-4b09-836e-be6e15f02cff"
+}
