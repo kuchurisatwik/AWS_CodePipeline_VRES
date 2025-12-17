8be426dd34c6338427502eb2a8b655c35ccc1237
kuchurisatwik <kuchurisatwik@gmail.com>
2025-12-17 12:20:25 +0530

    main modified

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
-# =========================================================================
 # 1. FRONTEND INFRASTRUCTURE
-# =========================================================================
+
 
 module "infra" {
   source       = "./modules/infrastructure"
   project_name = var.project_name
 }
 
-# =========================================================================
+
 # 2. FRONTEND CI/CD PIPELINE
-# =========================================================================
+
 
 module "cicd" {
   source = "./modules/cicd"
@@ -32,20 +31,19 @@ module "cicd" {
   codepipeline_role_arn      = module.infra.codepipeline_role_arn
 }
 
-# =========================================================================
+
+
 # 3. BACKEND INFRASTRUCTURE
-#    (EC2 Instance, Security Groups, IAM Instance Profile)
-# =========================================================================
+
 module "backend_infra" {
   source        = "./modules/backend_infra"
   project_name  = var.project_name
   aws_region    = var.aws_region
 }
 
-# =========================================================================
+
 # 4. BACKEND CI/CD PIPELINE
-#    (CodePipeline -> CodeBuild -> CodeDeploy -> EC2)
-# =========================================================================
+
 module "backend_cicd" {
   source = "./modules/backend_cicd"
 
